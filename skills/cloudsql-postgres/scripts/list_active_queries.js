4681ff9b8f22ce900a2d783b6d001325a822b648
Twisha Bansal <twishabansal07@gmail.com>
2026-03-10 12:03:14 +0530

    add env loading require

--- skills/cloudsql-postgres/scripts/list_active_queries.js
+++ skills/cloudsql-postgres/scripts/list_active_queries.js
@@ -14,6 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+require('./load_env');
+
 const { spawn, execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
