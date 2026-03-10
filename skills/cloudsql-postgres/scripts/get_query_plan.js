741c9adb6453ce5d8cf75341ae18628fadb4af2a
Twisha Bansal <twishabansal07@gmail.com>
2026-03-10 21:11:26 +0530

    remove load env file

--- skills/cloudsql-postgres/scripts/get_query_plan.js
+++ skills/cloudsql-postgres/scripts/get_query_plan.js
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-require('./load_env');
+
 
 const { spawn, execSync } = require('child_process');
 const path = require('path');
