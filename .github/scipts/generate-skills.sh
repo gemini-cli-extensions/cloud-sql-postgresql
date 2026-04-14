bd125460bf5b1c061aa1fda6ea7b7ad7c516c5a6
Twisha Bansal <58483338+twishabansal@users.noreply.github.com>
2026-04-14 10:46:46 +0530

    Update generate-skills.sh

--- .github/scipts/generate-skills.sh
+++ .github/scipts/generate-skills.sh
@@ -95,3 +95,17 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License." --additional-notes="Note: The scripts automatically load the environment variables from various .env files. Do not ask the user to set vars unless skill executions fails due to env var absence."
+
+./toolbox2 --prebuilt cloud-sql-postgres skills-generate --name "cloud-sql-postgres-vectorassist" --description "Use these skills to set up and optimize production-ready vector workloads by simply expressing your intent and performance requirements" --toolset=vectorassist  --license-header "// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the \"License\");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an \"AS IS\" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License." --additional-notes="Note: The scripts automatically load the environment variables from various .env files. Do not ask the user to set vars unless skill executions fails due to env var absence."
